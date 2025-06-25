576f3a68ebd0fb9e5a2854021eee750b1f98336e
Stephen Chung <spchung95@gmail.com>
2025-06-25 15:38:34 -0400

    use Bunfile

--- lib/janitor/index.ts
+++ lib/janitor/index.ts
@@ -2,12 +2,6 @@ import { join } from "path";
 import { readdir, stat, rename, mkdir, exists } from "fs/promises";
 import { Stats } from "fs";
 
-interface JanitorFile {
-  name: string; // slug
-  path: string; // full path
-  stats: Stats;
-}
-
 export class Janitor {
   constructor(
     /**
@@ -29,8 +23,9 @@ export class Janitor {
   async archive({ olderThanMs }: { olderThanMs: number }): Promise<number> {
     const allFiles = await this.scan(this.baseDirectory);
 
-    const filesToArchive = allFiles.filter((file) => {
-      return file.stats.mtime.getTime() < Date.now() - olderThanMs;
+    const filesToArchive = allFiles.filter(async (file) => {
+      const stat = await file.stat();
+      return stat.mtime.getTime() < Date.now() - olderThanMs;
     });
 
     if (filesToArchive.length === 0) {
@@ -44,24 +39,27 @@ export class Janitor {
     const archiveEntry = await this.createArchiveEntry();
 
     for (const file of filesToArchive) {
-      const destinationPath = join(archiveEntry, file.name);
-      await rename(file.path, destinationPath);
+      if (file.name) {
+        const slug = file.name.split("/").at(-1);
+        if (!slug) {
+          console.error("No slug found for file", file.name);
+          continue;
+        }
+        const destinationPath = join(archiveEntry, slug);
+        await rename(file.name, destinationPath);
+      }
     }
 
     return filesToArchive.length;
   }
 
   // scans a directory for files that match @patterns
-  private async scan(dir: string): Promise<JanitorFile[]> {
-    const files: JanitorFile[] = [];
+  private async scan(dir: string): Promise<Bun.BunFile[]> {
+    const files: Bun.BunFile[] = [];
 
     for (const fileName of await readdir(dir)) {
       if (this.patterns.some((pattern) => pattern.test(fileName))) {
-        files.push({
-          name: fileName,
-          path: join(dir, fileName),
-          stats: await stat(join(dir, fileName)),
-        });
+        files.push(Bun.file(join(dir, fileName)));
       }
     }
 
