95f7c1bed67c6fda68ca8574b18935954e332811
Stephen Chung <spchung95@gmail.com>
2025-06-25 15:16:30 -0400

    more config

--- config.example.ts
+++ config.example.ts
@@ -1,6 +1,20 @@
+import { join } from "path";
 import { duration } from "./lib/constants";
+
+const baseDirectory = join(process.env.HOME || "", "Desktop");
+const archiveDirectory = join(
+  process.env.HOME || "",
+  "Desktop",
+  "janitor-archive"
+);
 
 export const config = {
   screenshotThresholdMs: duration.OneMinute.ms * 30,
   defaultCron: "0 * * * *", // every hour
+  patterns: [
+    // Screenshot names on MacOS have non-standard whitespace characters (like \u202F, a non-breaking space)
+    /^Screenshot \d{4}-\d{2}-\d{2} at \d{1,2}\.\d{2}\.\d{2}[\u00A0\u202F]?(AM|PM)/i,
+  ],
+  baseDirectory,
+  archiveDirectory,
 };
--- index.ts
+++ index.ts
@@ -1,22 +1,13 @@
 import { config } from "./config";
 import { duration } from "./lib/constants";
 import { Janitor } from "./lib/janitor";
-import { join } from "path";
 
 async function main(): Promise<void> {
-  const baseDirectory = join(process.env.HOME || "", "Desktop");
-  const archiveDirectory = join(
-    process.env.HOME || "",
-    "Desktop",
-    "janitor-archive"
+  const janitor = new Janitor(
+    config.baseDirectory,
+    config.patterns,
+    config.archiveDirectory
   );
-  const patterns = [
-    // E.g, matches "Screenshot 2025-06-22 at 5.15.40 PM"
-    // Screenshot names on MacOS have non-standard whitespace characters (like \u202F, a non-breaking space)
-    /^Screenshot \d{4}-\d{2}-\d{2} at \d{1,2}\.\d{2}\.\d{2}[\u00A0\u202F]?(AM|PM)/i,
-  ];
-
-  const janitor = new Janitor(baseDirectory, patterns, archiveDirectory);
 
   const numArchived = await janitor.archive({
     olderThanMs: config.screenshotThresholdMs,
